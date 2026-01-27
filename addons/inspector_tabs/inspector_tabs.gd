afcf6a39373e5ca5be6d6cf1c80eb3359cfe2667
PiCode <122909224+PiCode9560@users.noreply.github.com>
2026-01-27 10:56:19 +0300

    Improved tab bar vertical mode for better stability and performance.
    Updated version to 0.8.2-dev.

--- addons/inspector_tabs/inspector_tabs.gd
+++ addons/inspector_tabs/inspector_tabs.gd
@@ -23,10 +23,12 @@ var tabs = [] # All tabs in the inspector
 
 var categories_finish = false # Finish adding categories
 
-var tab_bar:TabBar # Inspector Tabs
+var inspector_tab_bar:InspectorTabBar # Inspector Tabs
 var base_control = EditorInterface.get_base_control()
 var settings = EditorInterface.get_editor_settings()
 
+## Container to place the inspector and the tab bar.
+var inspector_container :BoxContainer
 
 var tab_can_change = false # Stops the TabBar from changing tab
 
@@ -38,10 +40,6 @@ var merge_abstract_class_tabs:bool
 
 ## path to the editor inspector list of properties
 var property_container = EditorInterface.get_inspector().get_child(0).get_child(2)
-## path to the editor inspector favorite list.
-var favorite_container = EditorInterface.get_inspector().get_child(0).get_child(1)
-## path to the editor inspector "viewer" area. (camera viewer or skeleton3D bone tree)
-var viewer_container = EditorInterface.get_inspector().get_child(0).get_child(0)
 ## path to the inspector scroll bar
 var property_scroll_bar:VScrollBar = EditorInterface.get_inspector().get_node("_v_scroll")
 ## Use to check if the loaded icon is an unknown icon
@@ -107,12 +105,12 @@ func _parse_end(object: Object) -> void:
 	if tab == -1:
 		_switch_tab(0)
 		tab_selected(0)
-		tab_bar.current_tab = 0
+		inspector_tab_bar.tab_bar.current_tab = 0
 	else:
 		_switch_tab(tab)
-		tab_bar.current_tab = tab
+		inspector_tab_bar.tab_bar.current_tab = tab
 
-	tab_resized()
+
 
 # Start inspector loading
 func parse_begin(object: Object) -> void:
@@ -120,46 +118,22 @@ func parse_begin(object: Object) -> void:
 	categories.clear()
 	tabs.clear()
 	tab_can_change = false
-	tab_bar.clear_tabs()
+	inspector_tab_bar.tab_bar.clear_tabs()
 
 func process(delta) -> void:
-	# Reposition UI
-	if vertical_mode:
-		tab_bar.size.x = EditorInterface.get_inspector().size.y
-		if vertical_tab_side == 0:#Left side
-			tab_bar.global_position = EditorInterface.get_inspector().global_position+Vector2(0,tab_bar.size.x)
-			tab_bar.rotation = -PI/2
-			property_container.custom_minimum_size.x = property_container.get_parent_area_size().x - tab_bar.size.y - 5
-			favorite_container.custom_minimum_size.x = favorite_container.get_parent_area_size().x - tab_bar.size.y - 5
-			viewer_container.custom_minimum_size.x = favorite_container.get_parent_area_size().x - tab_bar.size.y - 5
-			property_container.position.x = tab_bar.size.y + 5
-			favorite_container.position.x = tab_bar.size.y + 5
-			viewer_container.position.x = tab_bar.size.y + 5
-		else:#Right side
-			tab_bar.global_position = EditorInterface.get_inspector().global_position+Vector2(favorite_container.get_parent_area_size().x+tab_bar.size.y/2,0)
-			if property_scroll_bar.visible:
-				property_scroll_bar.position.x = property_container.get_parent_area_size().x - tab_bar.size.y+property_scroll_bar.size.x/2
-				tab_bar.global_position.x += property_scroll_bar.size.x
-			tab_bar.rotation = PI/2
-			property_container.custom_minimum_size.x = property_container.get_parent_area_size().x - tab_bar.size.y - 5
-			favorite_container.custom_minimum_size.x = favorite_container.get_parent_area_size().x - tab_bar.size.y - 5
-			viewer_container.custom_minimum_size.x = favorite_container.get_parent_area_size().x - tab_bar.size.y - 5
-			property_container.position.x = 0
-			favorite_container.position.x = 0
-			viewer_container.position.x = 0
 
 	if EditorInterface.get_inspector().global_position.x < base_control.get_viewport().size.x/2 -EditorInterface.get_inspector().size.x/2:
 		if vertical_tab_side != 1:
 			vertical_tab_side = 1
-			change_vertical_mode()
+			_update_tabbar()
 	else:
 		if vertical_tab_side != 0:
 			vertical_tab_side = 0
-			change_vertical_mode()
+			_update_tabbar()
 
-	if tab_bar.tab_count != 0:
+	if inspector_tab_bar.tab_bar.tab_count != 0:
 		if EditorInterface.get_inspector().get_edited_object() == null:
-			tab_bar.clear_tabs()
+			inspector_tab_bar.tab_bar.clear_tabs()
 
 
 ## Start plugin
@@ -167,8 +141,23 @@ func start() -> void:
 	property_scroll_bar.scrolling.connect(property_scrolling)
 
 	var filter_bar = inspector_dock.find_children("","LineEdit",true,false).filter(func(line_edit:LineEdit): return line_edit.right_icon == EditorInterface.get_editor_theme().get_icon("Search","EditorIcons"))[0]
-
 	filter_bar.text_changed.connect(_filter_text_changed)
+
+	var inspector := EditorInterface.get_inspector()
+	inspector_container = BoxContainer.new()
+	inspector.get_parent().add_child(inspector_container)
+
+	inspector_tab_bar = InspectorTabBar.new(vertical_mode)
+	inspector_container.add_child(inspector_tab_bar)
+	inspector_tab_bar.mouse_filter = Control.MOUSE_FILTER_PASS
+	inspector_tab_bar.tab_bar.tab_selected.connect(tab_selected)
+	inspector_tab_bar.tab_bar.tab_changed.connect(_on_tab_changed)
+
+	inspector.reparent(inspector_container)
+	_update_tabbar()
+
+	inspector.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	inspector.size_flags_vertical = Control.SIZE_EXPAND_FILL
 
 	var settings = EditorInterface.get_editor_settings()
 	tab_style = settings.get("inspector_tabs/tab_style")
@@ -184,14 +173,9 @@ func start() -> void:
 			change_vertical_mode(true)
 
 func exit() -> void:
-	property_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	favorite_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	viewer_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	property_container.custom_minimum_size.x = 0
-	favorite_container.custom_minimum_size.x = 0
-	viewer_container.custom_minimum_size.x = 0
-
-	tab_bar.queue_free()
+	EditorInterface.get_inspector().reparent(inspector_container.get_parent())
+	inspector_container.queue_free()
+	inspector_tab_bar.queue_free()
 
 # Is it not a custom class
 func is_base_class(c_name:String) -> bool:
@@ -228,7 +212,7 @@ func get_script_icon(script_path:String) -> Texture2D:
 
 # add tabs
 func update_tabs() -> void:
-	tab_bar.clear_tabs()
+	inspector_tab_bar.tab_bar.clear_tabs()
 	for tab:String in tabs:
 		## Get an icon for the tab.
 		var load_icon = get_tab_icon(tab)
@@ -247,24 +231,24 @@ func update_tabs() -> void:
 
 		match tab_style:
 			TabStyles.TEXT_ONLY:
-				tab_bar.add_tab(tab_name,null)
+				inspector_tab_bar.tab_bar.add_tab(tab_name,null)
 			TabStyles.ICON_ONLY:
-				tab_bar.add_tab("",load_icon)
+				inspector_tab_bar.tab_bar.add_tab("",load_icon)
 			TabStyles.TEXT_AND_ICON:
-				tab_bar.add_tab(tab_name,load_icon)
+				inspector_tab_bar.tab_bar.add_tab(tab_name,load_icon)
 
-		tab_bar.set_tab_tooltip(tab_bar.tab_count-1,tab_name)
+		inspector_tab_bar.tab_bar.set_tab_tooltip(inspector_tab_bar.tab_bar.tab_count-1,tab_name)
 
 func _on_tab_changed(tab: int) -> void:
 	_update_inspector()
 
 func _switch_tab(tab: int) -> void:
-	tab_bar.current_tab = tab
+	inspector_tab_bar.tab_bar.current_tab = tab
 
 func _update_inspector() -> void:
 	if is_filtering: return
 
-	var tab := tab_bar.current_tab
+	var tab := inspector_tab_bar.tab_bar.current_tab
 	if property_mode == TabPropertyModes.TABBED: # Tabbed
 		var category_idx = -1
 		var tab_idx = -1
@@ -320,77 +304,37 @@ func _filter_text_changed(text:String):
 		is_filtering = true
 	else:
 		is_filtering = false
-		_switch_tab(tab_bar.current_tab)
+		_switch_tab(inspector_tab_bar.tab_bar.current_tab)
 
 
 func tab_selected(tab):
 	if tab_can_change:
 		current_category = tabs[tab]
 
-func tab_resized():
-	if not vertical_mode:
-		if tabs.size() != 0:
-			tab_bar.max_tab_width = tab_bar.get_parent().get_rect().size.x/tabs.size()
 
 
 
 # Change position mode
-func change_vertical_mode(mode:bool = vertical_mode):
+func change_vertical_mode(mode:bool):
 	vertical_mode = mode
-	if tab_bar:
-		tab_bar.queue_free()
-	vertical_mode = vertical_mode
+	_update_tabbar()
 
-	tab_bar = TabBar.new()
-	tab_bar.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	tab_bar.clip_tabs = true
-	tab_bar.rotation = PI/2
-	tab_bar.mouse_filter =Control.MOUSE_FILTER_PASS
-	var panel = Panel.new()
-	tab_bar.add_child(panel)
-	panel.anchor_right = 1
-	panel.anchor_bottom = 1
-	panel.show_behind_parent = true
-	panel.mouse_filter = Control.MOUSE_FILTER_IGNORE
-
-	tab_bar.tab_changed.connect(_on_tab_changed)
+func _update_tabbar() -> void:
+	if inspector_tab_bar.get_index() != vertical_tab_side:
+		inspector_container.move_child(inspector_tab_bar,vertical_tab_side)
 
 	if not vertical_mode:
-		###### Find the first ancestor VBoxContainer function
-		var find_parent_vbox := func(from_node:Node, find_parent_vbox:Callable) -> VBoxContainer:
-			var parent := from_node.get_parent()
-			if parent is VBoxContainer:
-				return parent
-			return find_parent_vbox.call(parent, find_parent_vbox)
-		find_parent_vbox = find_parent_vbox.bind(find_parent_vbox)
-		######
+		inspector_container.move_child(inspector_tab_bar,0)
 
-		var inspector_parent_vbox = find_parent_vbox.call(EditorInterface.get_inspector())
-		inspector_parent_vbox.add_child(tab_bar)
-		inspector_parent_vbox.move_child(tab_bar,inspector_parent_vbox.get_child_count() - 2)
+	inspector_container.vertical = !vertical_mode
+
+	inspector_tab_bar.change_vertical_mode(vertical_mode)
+	inspector_tab_bar.change_vertical_side(vertical_tab_side)
 
 	update_tabs()
 
-	if vertical_mode:
-		EditorInterface.get_inspector().add_child(tab_bar)
-		property_container.size_flags_horizontal = Control.SIZE_SHRINK_END
-		favorite_container.size_flags_horizontal = Control.SIZE_SHRINK_END
-		viewer_container.size_flags_horizontal = Control.SIZE_SHRINK_END
-		tab_bar.top_level = true
-		if vertical_tab_side == 0:
-			tab_bar.layout_direction =Control.LAYOUT_DIRECTION_RTL
-		else:
-			tab_bar.layout_direction =Control.LAYOUT_DIRECTION_LTR
-	else:
-		property_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-		favorite_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-		viewer_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-		property_container.custom_minimum_size.x = 0
-		favorite_container.custom_minimum_size.x = 0
-		viewer_container.custom_minimum_size.x = 0
-	tab_bar.resized.connect(tab_resized)
-	tab_bar.tab_selected.connect(tab_selected)
-	tab_resized()
+
+
 
 
 func settings_changed() -> void:
@@ -430,13 +374,15 @@ func settings_changed() -> void:
 		if err != OK:
 			print("Error saving inspector tab settings: ",error_string(err))
 
+	_update_tabbar()
+
 func property_scrolling():
-	if property_mode != TabPropertyModes.JUMP_SCROLL or tab_bar.tab_count == 0 or is_filtering:return
+	if property_mode != TabPropertyModes.JUMP_SCROLL or inspector_tab_bar.tab_bar.tab_count == 0 or is_filtering:return
 	var category_idx = -1
 	var tab_idx = -1
 	var category_y = - INF
 	if property_scroll_bar.value <= 1:
-		tab_bar.current_tab = 0
+		inspector_tab_bar.tab_bar.current_tab = 0
 		return
 	for i in property_container.get_children():
 		if i.get_class() == "EditorInspectorCategory":
@@ -444,7 +390,7 @@ func property_scrolling():
 			if (i.position.y+property_container.position.y-EditorInterface.get_inspector().size.y/3) <= property_scroll_bar.value/property_scroll_bar.max_value*list_size_y:
 				category_y = property_container.position.y
 			else:
-				tab_bar.current_tab = max(tab_idx,0)
+				inspector_tab_bar.tab_bar.current_tab = max(tab_idx,0)
 				break
 			category_idx += 1
 			if is_new_tab(categories[category_idx]):
@@ -547,3 +493,70 @@ func load_gdextension_config(path: String) -> Dictionary:
 			data[section][key] = config.get_value(section, key)
 
 	return data
+
+
+class InspectorTabBar extends Control:
+	var tab_bar:TabBar
+	var _panel: Panel
+
+	var vertical_mode := false
+	var vertical_side := 1 ## 0:left; 1:Right;
+
+	func _init(vertical: bool) -> void:
+		vertical_mode = vertical
+		self.name = "InspectorTabBar"
+
+		_panel = Panel.new()
+		add_child(_panel)
+		_panel.set_anchors_preset(Control.PRESET_FULL_RECT)
+		_panel.clip_contents = true
+		_panel.owner = self
+
+		tab_bar = TabBar.new()
+		_panel.add_child(tab_bar)
+		tab_bar.owner = self
+
+		tab_bar.minimum_size_changed.connect(_on_tabbar_minimum_size_changed)
+		resized.connect(_on_resized)
+		_update_tabbar()
+
+	func _update_tabbar() -> void:
+		if vertical_mode:
+			if vertical_side == 0:
+				tab_bar.rotation = -PI/2
+				tab_bar.size = Vector2(size.y, size.x)
+				tab_bar.layout_direction = Control.LAYOUT_DIRECTION_RTL
+				tab_bar.position = Vector2(0, size.y)
+			elif vertical_side == 1:
+
+				tab_bar.rotation = PI/2
+				tab_bar.size = Vector2(size.y, size.x)
+				tab_bar.layout_direction = Control.LAYOUT_DIRECTION_LTR
+				tab_bar.position = Vector2(size.x, 0)
+
+		else:
+			tab_bar.layout_direction = Control.LAYOUT_DIRECTION_INHERITED
+			tab_bar.position = Vector2.ZERO
+			tab_bar.rotation = 0
+			tab_bar.size = Vector2(size.x, size.y)
+		_update_minimum_size()
+
+	func _on_tabbar_minimum_size_changed() -> void:
+		_update_tabbar()
+
+	func _on_resized() -> void:
+		_update_tabbar()
+
+	func _update_minimum_size() -> void:
+		if vertical_mode:
+			custom_minimum_size = Vector2(tab_bar.get_minimum_size().y,tab_bar.get_minimum_size().x)
+		else:
+			custom_minimum_size = Vector2(tab_bar.get_minimum_size().x,tab_bar.get_minimum_size().y)
+
+	func change_vertical_mode(vertical: bool) -> void:
+		vertical_mode = vertical
+		_update_tabbar()
+
+	func change_vertical_side(side: int) -> void:
+		vertical_side = side
+		_update_tabbar()
